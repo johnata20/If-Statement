5f1b17931615a335b6c62e929e81964a166e7b70
johnata20 <114643834+johnata20@users.noreply.github.com>
2022-10-01 13:37:01 -0600

    Add files via upload

---
+++ If Statement.cs
@@ -0,0 +1,28 @@
+﻿int order;
+Console.WriteLine("These are all the available beverages:");
+Console.WriteLine("1. Coke");
+Console.WriteLine("2. Sprite");
+Console.WriteLine("3. Royal Orange");
+Console.WriteLine("4. Moutain Dew");
+Console.WriteLine("5. Mineral Water");
+
+Console.WriteLine();
+Console.Write("Enter the number of your selection:");
+order = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("You have selected:");
+
+if (order == 1)
+    Console.WriteLine("Coke");
+if (order == 2)
+    Console.WriteLine("Sprite");
+if (order == 3)
+    Console.WriteLine("Royal Orange");
+if (order == 4)
+    Console.WriteLine("Mountain Dew");
+if (order == 5)
+    Console.WriteLine("Mineral Water");
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit....");
+Console.ReadLine();
